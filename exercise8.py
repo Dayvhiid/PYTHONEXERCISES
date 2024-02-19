86c3dedc28d1fb8b6c86d799b04662d3c6d421c5
Dada <daviddada360@gmail.com>
2024-02-19 09:28:01 +0100

    more exercises

---
+++ exercise8.py
@@ -0,0 +1,7 @@
+def palindrome(word):
+    if word[::-1] == word:
+        print("It is a plaindrome")
+    else :
+        print("It is not a plaindrom")  
+        
+palindrome("121")
---
+++ exercise9.py
@@ -0,0 +1,7 @@
+def is_member(ele1,ele2):
+    for _ in ele2:
+        if _ in ele1:
+            return print("True")
+        else :
+            return print("FAlse")
+is_member("1234","957")
